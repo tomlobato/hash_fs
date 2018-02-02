51efbc0424481ec3825503fd0e754bb6389826f2
Tom Lobato <tomlobato@gmail.com>
2018-02-01 22:23:46 -0200

    cleanup

--- make.sh
+++ make.sh
@@ -6,8 +6,8 @@ GREEN='\e[32m'
 NC='\e[0m'
 
 cd `dirname $0`
-# kmod
-for tgt in fsck mkfs debugfs ; do
+
+for tgt in fsck mkfs debugfs kmod; do
     echo -e "${CYAN}==== $tgt ${NC}"
 
     cd $tgt && \
