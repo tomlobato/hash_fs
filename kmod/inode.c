24c4789ad75529e856f45e8bcb44295892a96f36
Tom Lobato <tomlobato@gmail.com>
2018-02-06 18:18:35 -0200

    create

--- kmod/inode.c
+++ kmod/inode.c
@@ -25,28 +25,12 @@ void hashfs_fill_inode(struct super_block *sb, struct inode *inode,
     }
 }
 
-static inline int hashfs_add_nondir(struct dentry *dentry, struct inode *inode)
-{
-		unlock_new_inode(inode);
-		d_instantiate(dentry, inode);
-		return 0;
-	
-}
+int hashfs_save(struct super_block *sb, struct inode *inode, struct dentry * dentry, umode_t mode){
+    struct hashfs_inode *h_inode;
 
-struct inode *hashfs_new_inode(struct inode *dir, umode_t mode)
-{
-	struct super_block *sb;
-	struct inode * inode;
-	int err;
-	struct hashfs_inode *h_inode;
+    h_inode = kmem_cache_alloc(hashfs_inode_cache, GFP_KERNEL);
 
-	sb = dir->i_sb;
-	inode = new_inode(sb);
-	if (!inode)
-		return ERR_PTR(-ENOMEM);
-
-	h_inode = kmem_cache_alloc(hashfs_inode_cache, GFP_KERNEL);
-	h_inode->ino = 2;
+    h_inode->ino = 2;
 	h_inode->name_size = 8;
 	h_inode->i_links_count = 1;
 
@@ -54,32 +38,115 @@ struct inode *hashfs_new_inode(struct inode *dir, umode_t mode)
 	inode->i_blocks = 0;
 	hashfs_fill_inode(sb, inode, h_inode);
 
+    kmem_cache_free(hashfs_inode_cache, h_inode);
+
+    return 0;
+}
+
+int hashfs_create (struct inode * dir, struct dentry * dentry, umode_t mode, bool excl)
+{
+	struct super_block *sb;
+	struct inode * inode;
+	int err;
+
+	sb = dir->i_sb;
+	inode = new_inode(sb);
+	if (!inode)
+		return ENOMEM;
+
+    if ((err = hashfs_save(sb, inode, dentry, mode)))
+        return err;
+    
 	if (insert_inode_locked(inode) < 0) {
 		printk(KERN_DEBUG "hashfs_new_inode OPS");
 		// hashfs_error(sb, "hashfs_new_inode",
 		// 	   "inode number already in use - inode=%lu",
 		// 	   (unsigned long) ino);
-		err = -EIO;
 		make_bad_inode(inode);
 		iput(inode);
-		return ERR_PTR(err);
+		return EIO;
 	}
 
-	mark_inode_dirty(inode);
-	return inode;
-}
-
-int hashfs_create (struct inode * dir, struct dentry * dentry, umode_t mode, bool excl)
-{
-	struct inode *inode;
-
-	inode = hashfs_new_inode(dir, mode);
 	if (IS_ERR(inode))
 		return PTR_ERR(inode);
 
 	mark_inode_dirty(inode);
-	return hashfs_add_nondir(dentry, inode);
+
+    unlock_new_inode(inode);
+    d_instantiate(dentry, inode);
+
+    return 0;
 }
+
+struct dentry *hashfs_lookup(struct inode *dir,
+                              struct dentry *dentry,
+                              unsigned int flags) {
+    struct hashfs_inode *h_inode;
+    struct hashfs_superblock *hs;
+    struct inode *inode = NULL;
+    struct super_block *sb;
+    struct buffer_head *bh_bitm = NULL, 
+                        *bh_hash = NULL, 
+                        *bh_ino = NULL;
+    uint64_t hash_slot, inode_offset_byte = 0;
+    uint8_t *bitm_byte;
+    void *hash_key_ptr;
+
+    printk(KERN_DEBUG "hashfs_lookup dir=%lu fname=%.*s dentry=%p\n", dir->i_ino, 
+                dentry->d_name.len, dentry->d_name.name, dentry);
+
+    sb = dir->i_sb;
+    hs = sb->s_fs_info;
+
+    // hash_slot = HASH_SLOT(dentry->d_name.name, 
+    //                         dentry->d_name.len, hs->hash_len);
+
+    // READ_BYTES(sb, bh_bitm, bitm_byte, 
+    //             hs->bitmap_offset_blk, hash_slot / 8);
+
+    // if (HAS_BIT(*bitm_byte, hash_slot % 8)) {
+    //     READ_BYTES(sb, bh_hash, hash_key_ptr, 
+    //                 hs->hash_offset_blk, hash_slot * hs->hash_slot_size);
+    //     memcpy(&inode_offset_byte, hash_key_ptr, hs->hash_slot_size);
+
+    //     if (inode_offset_byte) {
+            inode = new_inode(sb);
+            if (!inode) {
+                printk(KERN_ERR "Cannot create new inode. No memory.\n");
+				if (bh_bitm != NULL) brelse(bh_bitm);
+				if (bh_hash != NULL) brelse(bh_hash);
+				if (bh_ino != NULL) brelse(bh_ino);
+                // goto leave;
+            } 
+            // READ_BYTES(sb, bh_ino, h_inode, 
+            //     hs->inodes_offset_blk, inode_offset_byte);
+			h_inode = kmem_cache_alloc(hashfs_inode_cache, GFP_KERNEL);
+			h_inode->ino = 2;
+            hashfs_fill_inode(sb, inode, h_inode);
+        // }
+    // }
+
+    // d_add(dentry, inode);
+
+// leave:
+    if (bh_bitm != NULL) brelse(bh_bitm);
+    if (bh_hash != NULL) brelse(bh_hash);
+    if (bh_ino != NULL) brelse(bh_ino);
+
+    // return dentry;
+	return d_splice_alias(inode, dentry);
+}
+
+void hashfs_save_hashfs_inode(struct super_block *sb,
+                                struct hashfs_inode *inode_buf) {
+    printk(KERN_DEBUG "hashfs_save_hashfs_inode\n");
+}
+
+void hashfs_destroy_inode(struct inode *inode) {
+    printk(KERN_DEBUG "hashfs_destroy_inode\n");
+    return;
+}
+
 
 /* TODO I didn't implement any function to dealloc hashfs_inode */
 // int hashfs_alloc_hashfs_inode(struct super_block *sb, uint64_t *out_inode_no) {
@@ -274,72 +341,52 @@ int hashfs_create (struct inode * dir, struct dentry * dentry, umode_t mode, boo
 //     return 0;
 // }
 
-struct dentry *hashfs_lookup(struct inode *dir,
-                              struct dentry *dentry,
-                              unsigned int flags) {
-    struct hashfs_inode *h_inode;
-    struct hashfs_superblock *hs;
-    struct inode *inode = NULL;
-    struct super_block *sb;
-    struct buffer_head *bh_bitm = NULL, 
-                        *bh_hash = NULL, 
-                        *bh_ino = NULL;
-    uint64_t hash_slot, inode_offset_byte = 0;
-    uint8_t *bitm_byte;
-    void *hash_key_ptr;
 
-    printk(KERN_DEBUG "hashfs_lookup dir=%lu fname=%.*s dentry=%p\n", dir->i_ino, 
-                dentry->d_name.len, dentry->d_name.name, dentry);
 
-    sb = dir->i_sb;
-    hs = sb->s_fs_info;
+// static inline int hashfs_add_nondir(struct dentry *dentry, struct inode *inode)
+// {
+// 		unlock_new_inode(inode);
+// 		d_instantiate(dentry, inode);
+// 		return 0;
+	
+// }
 
-    // hash_slot = HASH_SLOT(dentry->d_name.name, 
-    //                         dentry->d_name.len, hs->hash_len);
+// struct inode *hashfs_new_inode(struct inode *dir, umode_t mode)
+// {
+// 	struct super_block *sb;
+// 	struct inode * inode;
+// 	int err;
+// 	struct hashfs_inode *h_inode;
 
-    // READ_BYTES(sb, bh_bitm, bitm_byte, 
-    //             hs->bitmap_offset_blk, hash_slot / 8);
+// 	sb = dir->i_sb;
+// 	inode = new_inode(sb);
+// 	if (!inode)
+// 		return ERR_PTR(-ENOMEM);
 
-    // if (HAS_BIT(*bitm_byte, hash_slot % 8)) {
-    //     READ_BYTES(sb, bh_hash, hash_key_ptr, 
-    //                 hs->hash_offset_blk, hash_slot * hs->hash_slot_size);
-    //     memcpy(&inode_offset_byte, hash_key_ptr, hs->hash_slot_size);
+// 	h_inode = kmem_cache_alloc(hashfs_inode_cache, GFP_KERNEL);
+// 	h_inode->ino = 2;
+// 	h_inode->name_size = 8;
+// 	h_inode->i_links_count = 1;
 
-    //     if (inode_offset_byte) {
-            inode = new_inode(sb);
-            if (!inode) {
-                printk(KERN_ERR "Cannot create new inode. No memory.\n");
-				if (bh_bitm != NULL) brelse(bh_bitm);
-				if (bh_hash != NULL) brelse(bh_hash);
-				if (bh_ino != NULL) brelse(bh_ino);
-                // goto leave;
-            } 
-            // READ_BYTES(sb, bh_ino, h_inode, 
-            //     hs->inodes_offset_blk, inode_offset_byte);
-			h_inode = kmem_cache_alloc(hashfs_inode_cache, GFP_KERNEL);
-			h_inode->ino = 2;
-            hashfs_fill_inode(sb, inode, h_inode);
-        // }
-    // }
+// 	inode->i_ino = h_inode->ino;
+// 	inode->i_blocks = 0;
+// 	hashfs_fill_inode(sb, inode, h_inode);
 
-    // d_add(dentry, inode);
+// 	if (insert_inode_locked(inode) < 0) {
+// 		printk(KERN_DEBUG "hashfs_new_inode OPS");
+// 		// hashfs_error(sb, "hashfs_new_inode",
+// 		// 	   "inode number already in use - inode=%lu",
+// 		// 	   (unsigned long) ino);
+// 		err = -EIO;
+// 		make_bad_inode(inode);
+// 		iput(inode);
+// 		return ERR_PTR(err);
+// 	}
 
-// leave:
-    if (bh_bitm != NULL) brelse(bh_bitm);
-    if (bh_hash != NULL) brelse(bh_hash);
-    if (bh_ino != NULL) brelse(bh_ino);
+// 	mark_inode_dirty(inode);
+// 	return inode;
+// }
 
-    // return dentry;
-	return d_splice_alias(inode, dentry);
-}
-
-void hashfs_save_hashfs_inode(struct super_block *sb,
-                                struct hashfs_inode *inode_buf) {
-    printk(KERN_DEBUG "hashfs_save_hashfs_inode\n");
-}
-
-void hashfs_destroy_inode(struct inode *inode) {
-    printk(KERN_DEBUG "hashfs_destroy_inode\n");
-    return;
-}
-
+    // h_inode->ino = 2;
+	// h_inode->name_size = 8;
+	// h_inode->i_links_count = 1;
