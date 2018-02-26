83c93ff2b5379ebb6e2e5b0adea130e4d8527ebc
Tom Lobato <tomlobato@gmail.com>
2018-02-26 13:39:00 -0300

    pcache vs disk

--- kmod/k_util.c
+++ kmod/k_util.c
@@ -100,20 +100,33 @@ void hashfs_print_h_inode(char *point, struct hashfs_inode * ino){
     hashfs_trace("next \t %u\n", ino->next);
 }
 
-uint64_t _pcache_vs_disk(struct super_block *sb, struct hashfs_superblock *h_sb, int bnum, int *pos){
-    int i, 
-        s, 
-        j, 
+void pcache_vs_disk(struct super_block *sb, struct hashfs_superblock *h_sb){
+    unsigned 
+        *pos,
+        *ptr,
+        bnum = 100, 
+        i, 
         j_max,
-        *ptr;
+        j, 
+        k,
+        l,
+        s;
+    struct timeval t0, 
+                   t1;
+    uint64_t t[2],
+             temp;
     struct buffer_head *bh;
-    struct timeval t0, t1;
-    uint64_t temp;
 
     j_max = h_sb->blocksize / sizeof(int);
+    pos = kmalloc(bnum * sizeof(int), GFP_KERNEL);
 
-    // 
+    for(i = 0; i < bnum; i++) {
+        get_random_bytes(&k, sizeof(int));
+        pos[i] = k % (h_sb->block_count - 1000000); // bug on mkfs? why last 1M blocks cannot be accessed?
+    }
+    pr_info("block_count=%llu \n", h_sb->block_count);
 
+    // noop
     do_gettimeofday(&t0);
     s = 0;
     for(i = 0; i < bnum; i++) {
@@ -123,53 +136,32 @@ uint64_t _pcache_vs_disk(struct super_block *sb, struct hashfs_superblock *h_sb,
     }
     do_gettimeofday(&t1);
     temp = (t1.tv_sec - t0.tv_sec)*1000000 + (t1.tv_usec - t0.tv_usec);
-    pr_info("dt=%llu us (noop) %d \n", temp, s);
+    pr_info("noop=%lluus sum=%d \n", temp, s);
 
-    // 
+    // disk/pcache access
+    for(l = 0; l < 2; l++) {
+        do_gettimeofday(&t0);
+        s = 0;
+        for(i = 0; i < bnum; i++) {
+            bh = sb_bread(sb, h_sb->data_offset_blk + pos[i]);
+            if (!bh) {
+                pr_info("ops %d %d\n", i, pos[i]);
+                continue;
+            }
+            // BUG_ON(!bh);
+            // ptr = (int *)bh->b_data;
+            // for(j = 0; j < j_max; j++) {
+            //     s += *(ptr++);
+            // }
+            brelse(bh);
+        }
 
-    do_gettimeofday(&t0);
-    s = 0;
-    for(i = 0; i < bnum; i++) {
-        bh = sb_bread(sb, h_sb->data_offset_blk + pos[i]);
-        if (!bh) {
-            pr_info("ops %d %d\n", i, pos[i]);
-            continue;
-        }
-        // BUG_ON(!bh);
-        ptr = (int *)bh->b_data;
-        for(j = 0; j < j_max; j++) {
-            s += *(ptr++);
-        }
-        brelse(bh);
+        do_gettimeofday(&t1);
+        t[l] = (t1.tv_sec - t0.tv_sec)*1000000 + (t1.tv_usec - t0.tv_usec);
+        pr_info("l%d=%lluus sum=%d \n", l, t[l], s);
     }
 
-    do_gettimeofday(&t1);
-    temp = (t1.tv_sec - t0.tv_sec)*1000000 + (t1.tv_usec - t0.tv_usec);
-    pr_info("dt=%llu us %d \n", temp, s);
-
-    return temp;
-}
-
-void pcache_vs_disk(struct super_block *sb, struct hashfs_superblock *h_sb){
-    int bnum = 10000, 
-        *pos, 
-        i, 
-        k = 0;
-    uint64_t t,
-             t2;
-    
-    pos = kmalloc(bnum * sizeof(int), GFP_KERNEL);
-
-    for(i = 0; i < bnum; i++) {
-        get_random_bytes(&k, sizeof(int));
-        pos[i] = k % h_sb->block_count;
-    }
-    pr_info("pos[%d]=%d block_count=%llu \n", i, pos[0], h_sb->block_count);
-
-    t = _pcache_vs_disk(sb, h_sb, bnum, pos);
-    t2 = _pcache_vs_disk(sb, h_sb, bnum, pos);
-
-    pr_info("t/t2=%llu \n", t/t2);
+    pr_info("pcache/disk: %llux \n", t[0]/t[1]);
 
     kfree(pos);
 }
